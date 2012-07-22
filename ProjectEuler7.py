5d81ce744e69013cc044bc293d99f2e6e7a6b344
Jeff Nuss <jeffnuss@hotmail.com>
2012-07-21 23:39:26 -0600

    Got problem 7 correct. I used the simplest, most naive test for primality. See the first method at http://en.wikipedia.org/wiki/Primality_test for details.

---
+++ ProjectEuler7.py
@@ -0,0 +1,30 @@
+import math
+
+def main():
+    primes = [2]
+
+    startingNumber = 3
+    primeNumberToFind = 10001
+
+    while (len(primes)) != primeNumberToFind:
+        numToCheck = math.sqrt(startingNumber)
+        isPrime = True
+        # for prime in primes:
+        n = 0
+        prime = primes[n]
+        while prime < numToCheck:
+            prime = primes[n]
+            # print "Mod: " + str(numToCheck) + " % " + str(prime) + " = " + str(startingNumber % prime)
+            if (startingNumber % prime == 0):
+                isPrime = False
+                # print str(startingNumber) + " is not prime"
+            n += 1
+        if isPrime:
+            primes.append(startingNumber)
+        startingNumber += 2
+        # print startingNumber
+
+    print primes[primeNumberToFind - 1]
+
+if __name__ == "__main__":
+    main()
