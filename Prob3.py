d5cad29198d77ea128a69e4e0a4904b62c277cb7
Jeff Nuss <jeffnuss@hotmail.com>
2012-08-30 14:54:05 -0600

    Final touches on problem 3

--- Prob3.py
+++ Prob3.py
@@ -11,20 +11,20 @@
 #!/usr/bin/env python
 
 def main():
-    #target = 600851475143.0
+    target = 600851475143.0
     #target = 8462696833.0 #71
     #target = 10086647.0 #839
-    target = 6857.0 #1471
+    #target = 6857.0 #1471
     n = 2
     factors = []
     keepGoing = True
     while (keepGoing):
         if (target % n == 0):
             factors.append(n)
-            keepGoing = False
-            print(target / n)
-            print(n)
+            if (n == 1.0):
+                keepGoing = False
         n += 1
 
+    print(factors)
 if __name__ == '__main__':
     main()
